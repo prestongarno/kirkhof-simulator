ecdb920b0e3f0d3c938f7199a8ecc6224cced659
preston <prestongarno@gmail.com>
2017-03-23 09:53:04 -0400

    fixed directory mismatch

--- src/edu/gvsu/cis163/project4/ClockListener.java
+++
@@ -1,8 +0,0 @@
-package edu.gvsu.cis163.project4;
-
-/** **************************************************
- * @author Roger Ferguson
- * ***************************************************/
-public interface ClockListener {
-	public void event(int tick);  // the method that is called by the clock
-}
