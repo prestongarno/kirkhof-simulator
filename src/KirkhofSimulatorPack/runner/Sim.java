9759e91122cb2d0fd06e7e1fa734b56400e7f0e0
chadmv95 <vredevch@mail.gvsu.edu>
2017-04-02 19:51:58 -0400

    changed sim class and fixed errors

--- src/KirkhofSimulatorPack/runner/Sim.java
+++ src/KirkhofSimulatorPack/runner/Sim.java
@@ -14,10 +14,10 @@ public class Sim {
 	 **************************************************************/
 	public static void main(String[] args) {
 		/**Number of eateries in sim */
-		numEateries=4;
+		int numEateries=4;
 		Clock clk = new Clock();
 		//make an array of eateries
-		Eatery booth[numEateries] = new Eatery(mainQ);
+		Eatery eateryArray[numEateries];
 		MainQueue mainQ = new MainQueue();
 		
 		// 		int numOfTicksNextPerson = 20
@@ -25,21 +25,21 @@ public class Sim {
 		
 		//PersonProducer(eatery,numOfTicksNextPerson, 
 		//averageEateryTime, averageCashierTime, averageLeaveTime) 
-		PersonProducer produce = new PersonProducer(booth[], 20, 18,20,20);
+		PersonProducer produce = new PersonProducer(eateryArray[1], 20, 18,20,20);
 		clk.add(produce);
 		//clk.add(booth);
 				//create for loop to add clock to eateries
 				for(int i=0; i<numEateries;i++){
-					clk.add(booth[i]);
+					clk.add(eateryArray[i]);
 				}
 		clk.add(mainQ);
 	
 		clk.add(new ClockListener() {
 			@Override
 			public void event(int tick) {
-					System.out.println("Through put is: " + booth.getThroughPut() + " people.");
-					System.out.println("People that are still in the Q:" + booth.getLeft() + " people.");
-					System.out.println("Max Q length:" + booth.getMaxQlength() + " people.");
+					System.out.println("Through put is: " + eateryArray.getThroughPut() + " people.");
+					System.out.println("People that are still in the Q:" + eateryArray.getLeft() + " people.");
+					System.out.println("Max Q length:" + eateryArray.getMaxQlength() + " people.");
 					
 					//System.out.println("\nMain Queue Through put is: " + mainQ.getCompleted() + " people.");
 					System.out.println("People that are still in the main Q: " + mainQ.size() + " people.");
