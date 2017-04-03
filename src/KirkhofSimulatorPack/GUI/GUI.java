f469748d2598ed4745f4ef2ca7b1b5b1f510a386
chadmv95 <vredevch@mail.gvsu.edu>
2017-04-03 18:41:52 -0400

    started GUI and added an array of eateries to the Sim class

--- src/KirkhofSimulatorPack/GUI/GUI.java
+++ src/KirkhofSimulatorPack/GUI/GUI.java
@@ -8,11 +8,33 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+import java.text.*;
 
 /**
  * Created by Alex on 4/2/2017.
  */
-public class GUI implements KeyListener, ActionListener {
+public class GUI extends JFrame implements KeyListener, ActionListener  {
+
+    /**panel for the GUI elements to be placed on*/
+    JPanel panel;
+
+    /**panel to use for the main program graphics */
+    JPanel centerPanel;
+
+    public GUI(){
+        panel = new JPanel();
+        centerPanel = new JPanel();
+        centerPanel.setLayout(new GridLayout(5,5));
+        panel.setLayout(new BorderLayout());
+        panel.setVisible(true);
+        panel.setFocusable(true);
+
+
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
