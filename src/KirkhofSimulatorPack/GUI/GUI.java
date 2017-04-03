1cdc45a771709bee574ba966739826754c049593
Alex Radecki <alex_radecki@yahoo.com>
2017-04-02 20:03:41 -0400

    GUI

---
+++ src/KirkhofSimulatorPack/GUI/GUI.java
@@ -0,0 +1,35 @@
+package KirkhofSimulatorPack.GUI;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 4/2/2017.
+ */
+public class GUI implements KeyListener, ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
