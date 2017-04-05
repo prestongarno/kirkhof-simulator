0db20c33e738bbb09413494650fc9383c8e96326
Jessica <dargaj@mail.gvsu.edu>
2017-04-05 17:59:25 -0400

    Added structure

--- src/KirkhofSimulatorPack/GUI/GUI.java
+++ src/KirkhofSimulatorPack/GUI/GUI.java
@@ -2,6 +2,9 @@ package KirkhofSimulatorPack.GUI;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+
+import game1024.gamePanel.ButtonListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +17,7 @@ import java.text.*;
 
 /**
  * Created by Alex on 4/2/2017.
+ * Added beginning elements and structure Jessica on 4/5
  */
 public class GUI extends JFrame implements KeyListener, ActionListener  {
 
@@ -22,24 +26,85 @@ public class GUI extends JFrame implements KeyListener, ActionListener  {
 
     /**panel to use for the main program graphics */
     JPanel centerPanel;
+    
+    /** Panel contains game functions */
+	private JPanel panelLeft;
+
+	/** Panel contains game statistics */
+	private JPanel panelRight;
+	
+	/** Panel on top */
+	private JPanel panelUp;
+
+	/** Panel on bottom */
+	private JPanel panelDown;
+	
+	/**Button to add eateries */
+	private JButton addEatery;
+	
+	/**Button to remove eateries */
+	private JButton removeEatery;
 
     public GUI(){
         panel = new JPanel();
-        centerPanel = new JPanel();
-        centerPanel.setLayout(new GridLayout(5,5));
+        
         panel.setLayout(new BorderLayout());
         panel.setVisible(true);
         panel.setFocusable(true);
+       
+     	// creation of Center Panel
+     	//Center panel runs simulation
+     	centerPanel = new JPanel();
+        centerPanel.setLayout(new GridLayout(5,5));
+        
+        //creation of left panel
+        //Displays buttons text fields
+     	panelLeft= new JPanel();
+     	panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
 
+     	//Left Panel Elements
+     	addEatery = new JButton("Add Eatery");
+     	removeEatery= new JButton("Remove Eatery");
+     	
+     	//Left Panel Action Listeners
+     	//addEatery.addActionListener();
+     	//removeEatery.addActionListener();
+     	
+     	//Add elements to left panel
+        // Use of rigid area to create spacing between elements
+     	panelLeft.add(addEatery);
+     	panelLeft.add(removeEatery);
+     	
+		// creation of right panel
+		// Displays stats of simulation
+		panelRight = new JPanel();
+		panelRight
+		.setLayout(new BoxLayout(panelRight, BoxLayout.Y_AXIS));
+
+		// Up and Down panel Creation
+		panelUp = new JPanel();
+		panelUp.setLayout(new FlowLayout());
+		panelDown = new JPanel();
+		panelDown.setLayout(new FlowLayout());
+
+		// Add all panels to main panel
+		panel.add(panelUp, BorderLayout.NORTH);
+		panel.add(panelDown, BorderLayout.SOUTH);
+		panel.add(panelRight, BorderLayout.EAST);
+		panel.add(panelLeft, BorderLayout.WEST);
+		panel.add(centerPanel, BorderLayout.CENTER);
+		add(panel);
 
     }
 
 
-    @Override
+    
     public void actionPerformed(ActionEvent e) {
 
-    }
 
+    }
+    
+    
     @Override
     public void keyTyped(KeyEvent e) {
 
