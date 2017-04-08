e5fc0ecbc218b79d42a2352fc37eba167c3234d7
preston <prestongarno@gmail.com>
2017-04-08 11:05:18 -0400

    added main panel class

---
+++ src/KirkhofSimulatorPack/GUI/MainPanel.java
@@ -0,0 +1,30 @@
+package KirkhofSimulatorPack.GUI;
+
+import KirkhofSimulatorPack.Eatery;
+import sun.applet.Main;
+
+/**
+ * Created by preston on 4/8/17.
+ */
+public class MainPanel {
+
+    public MainPanel() {
+
+    }
+
+    public void addEatery(Eatery eatery) {
+
+    }
+
+    public void setQueue(String label) {
+
+    }
+
+    public void addCheckout() {
+
+    }
+
+    public void update() {
+
+    }
+}
