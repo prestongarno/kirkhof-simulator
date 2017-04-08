3487f32748fad0b7beab5cdd2c108e1da2065c34
Alex Radecki <alex_radecki@yahoo.com>
2017-04-08 10:59:27 -0400

    Added setters to regular person class

--- src/KirkhofSimulatorPack/RegularPerson.java
+++ src/KirkhofSimulatorPack/RegularPerson.java
@@ -5,4 +5,14 @@ package KirkhofSimulatorPack;
  */
 public class RegularPerson extends Person {
 
+    /**************************************************************
+     * Setter methods
+     * @param time The persons eatery/cashier/leave time
+     **************************************************************/
+    public void setEateryTime(double time) { super.setEateryTime(time); }
+
+    public void setCashierTime(double time) { super.setCashierTime(time); }
+
+    public void setLeaveTime(double time) { super.setLeaveTime(time); }
+
 }
