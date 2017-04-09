38b45609b3554180bb9314c573f43ee405ee8573
Preston Garno <prestongarno@gmail.com>
2017-04-09 18:59:48 -0400

    added main panel

--- src/KirkhofSimulatorPack/Eatery.java
+++ src/KirkhofSimulatorPack/Eatery.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * eatery and then moves to main Q
  * @author Chad, Jessica, Preston, Alex
  * @version 3/27/17
- **************************************************************/
+ ****************************************/
 public class Eatery implements ClockListener {
 	/**The line for the eatery*/
 	private ArrayList<Person> Q = new ArrayList<>();
@@ -24,35 +24,63 @@ public class Eatery implements ClockListener {
 	
 	/**The number of people who have gone through eatery*/
 	private int completed = 0;
-
+	
+	private String name;
 	/**Variable to represent Main Queue linked list*/
 	private final MainQueue mainList;
 
 	/**number of people who have lef at this eatery*/
 	private int totalPeopleLeft = 0;
 
-	/**
+	/*****************************************
 	 * Constructor for Eatery
 	 * @param mainLinkedList Reference to the Main Queue class
-	 */
+	 ****************************************/
 	public Eatery(MainQueue mainLinkedList){
 		this.mainList = mainLinkedList;
 	}
-
-	/**************************************************************
+	
+	/*****************************************
+	 * Constructor for Eatery
+	 * @param mainLinkedList Reference to the Main Queue class
+	 ****************************************/
+	public Eatery(String name, MainQueue mainLinkedList){
+		this.name = name;
+		this.mainList = mainLinkedList;
+	}
+	
+	/*****************************************
+	 * Getter for property 'name'.
+	 *
+	 * @return Value for property 'name'.
+	 ****************************************/
+	public String getName() {
+		return name;
+	}
+	
+	/*****************************************
+	 * Setter for property 'name'.
+	 *
+	 * @param name Value to set for property 'name'.
+	 ****************************************/
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*****************************************
 	 * Method to add people to eateries
 	 * @param person The person to be added to the eatery 
-	 **************************************************************/
+	 ****************************************/
 	public void add(Person person) {
 		Q.add(person);
 		if (Q.size() > maxQlength)
 			maxQlength = Q.size();
 	}
 
-	/**************************************************************
+	/*****************************************
 	 * Method to define the event of the clock
 	 * @param tick The current time of the clock
-	 **************************************************************/
+	 ****************************************/
 	public void event(int tick) {
 
 		if (tick >= timeOfNextEvent) {
@@ -85,38 +113,38 @@ public class Eatery implements ClockListener {
 			}
 		}
 	}
-	/**************************************************************
+	/*****************************************
 	 * The amount of people still in line
 	 * 
 	 * @return Q.size The amount of people in the q line
-	 **************************************************************/
+	 ****************************************/
 	public int getLeft() {
 		return Q.size();
 	}
 
-	/**************************************************************
+	/*****************************************
 	 * The maximum number of people in line
 	 * 
 	 * @return maxQlength
-	 **************************************************************/
+	 ****************************************/
 	public int getMaxQlength() {
 		return maxQlength;
 	}
 	
-	/**************************************************************
+	/*****************************************
 	 * Counter for the number of people going through the eatery
 	 * 
 	 * @return completed The number of people who have made it 
 	 * through the eatery
-	 **************************************************************/
+	 ****************************************/
 	public int getThroughPut() {
 		return completed;
 	}
 
-	/**
+	/*****************************************
 	 * getter for total people who have left
 	 * @return totalPeopleLeft number of people who have left at this eatery
-	 */
+	 ****************************************/
 	public int getTotalPeopleLeft() {
 		return totalPeopleLeft;
 	}
--- src/KirkhofSimulatorPack/GUI/MainPanel.java
+++ src/KirkhofSimulatorPack/GUI/MainPanel.java
@@ -3,28 +3,79 @@ package KirkhofSimulatorPack.GUI;
 import KirkhofSimulatorPack.Eatery;
 import sun.applet.Main;
 
-/**
+import javax.swing.*;
+import java.awt.*;
+
+
+/************************************
  * Created by preston on 4/8/17.
- */
-public class MainPanel {
-
-    public MainPanel() {
-
-    }
-
-    public void addEatery(Eatery eatery) {
-
-    }
-
-    public void setQueue(String label) {
-
-    }
-
-    public void addCheckout() {
-
-    }
-
-    public void update() {
-
-    }
+ ****************************************/
+public class MainPanel extends JPanel {
+	
+	
+	private final JLabel MAIN_QUEUE;
+	private final JPanel EATERIES;
+	private final JPanel CHECKOUTS;
+	
+	private JPanel[] eateryPanels;
+	private JPanel[] checkoutPanels;
+	
+	public MainPanel() {
+		// this is the the panel that holds all subpanels
+		setLayout(new BorderLayout(0, 0));
+		
+		//The layout for the eateries and the checkout
+		final GridLayout layout = new GridLayout(1, 10, 4, 4);
+		//JPanel that holds the Checkouts
+		CHECKOUTS = new JPanel(layout);
+		// JPanel that holds the Eateries
+		EATERIES = new JPanel(layout);
+		
+		// TODO: 4/9/17 next line
+		MAIN_QUEUE = new JLabel("What should the main queue be?");
+	}
+	
+	public void setQueue(String label) {
+		MAIN_QUEUE.setText(label);
+	}
+	
+	public void addEatery(Eatery eatery) {
+		JPanel eateryPanel = new JPanel(new BorderLayout(5, 5));
+		
+		// label for the eatery title, possibly replace with icon later
+		String label;
+		if (eatery.getName() == null) {
+			label = "Eatery #" + EATERIES.getComponents().length;
+		} else label = eatery.getName();
+		
+		// add the title of the eatery
+		eateryPanel.add(new JLabel(label), BorderLayout.CENTER);
+		// add the eatery "queue" label, todo possibly change this?
+		eateryPanel.add(new JLabel("" + eatery.getTotalPeopleLeft()), BorderLayout.SOUTH);
+		
+		this.eateryPanels = (JPanel[]) EATERIES.getComponents();
+	}
+	
+	
+	public void addCheckout() {
+		JPanel checkoutPanel = new JPanel(new BorderLayout(5, 5));
+		
+		String label;
+		if (checkoutPanel.getName() == null) {
+			label = "Eatery #" + EATERIES.getComponents().length;
+		} else label = checkoutPanel.getName();
+		
+		// add the title of the checkout
+		checkoutPanel.add(new JLabel(label), BorderLayout.CENTER);
+		
+		this.checkoutPanels = (JPanel[]) CHECKOUTS.getComponents();
+	}
+	
+	public void update() {
+		for (JPanel panel : this.checkoutPanels) {
+			//update checkouts
+		}
+		
+		//same foreach for the eateries
+	}
 }
